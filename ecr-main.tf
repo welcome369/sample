16214aac7adfefc0abee55c3ac989b8185235d0e
welcome369 <129384896+welcome369@users.noreply.github.com>
2026-01-19 19:30:14 +0530

    Update ecr-main.tf

--- ecr-main.tf
+++ ecr-main.tf
@@ -2,25 +2,6 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "demo-terraform-eks-state-s3-bucket-3699"
-
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-eks-state-lock-12369"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 resource "aws_ecr_repository" "aws_ecr" {
   name = "node-images"
 }
